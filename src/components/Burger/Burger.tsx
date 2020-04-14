807ae475b693646b59c0b22ba1b852dbb12b1341
dandelionadia <nadia.ridko@gmail.com>
2020-04-14 11:29:15 +0200

    Burger: adds burger component

--- src/components/Burger/Burger.tsx
+++ src/components/Burger/Burger.tsx
@@ -1,1 +1,40 @@
 import React from "react";
+import styled from "styled-components";
+import { BurgerIngredient } from "./BurgerIngredient/BurgerIngredient";
+
+const BurgerStyled = styled.div`
+  width: 100%;
+  margin: auto;
+  height: 250px;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+
+  @media (min-width: 500px) and (min-height: 400px) {
+    width: 350px;
+    height: 300px;
+  }
+
+  @media (min-width: 500px) and (min-height: 401px) {
+    width: 450px;
+    height: 400px;
+  }
+
+  @media (min-width: 1000px) and (min-height: 700px) {
+    width: 700px;
+    height: 600px;
+  }
+`;
+
+export const Burger = () => {
+  return (
+    <>
+      <BurgerStyled>
+        <BurgerIngredient type="bread-top" />
+        <BurgerIngredient type="cheese" />
+        <BurgerIngredient type="meat" />
+        <BurgerIngredient type="bread-bottom" />
+      </BurgerStyled>
+    </>
+  );
+};
